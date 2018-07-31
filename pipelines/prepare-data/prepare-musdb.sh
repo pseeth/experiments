5a68e6af3da4ad7156ba54546bb26f10ea79e159
Prem Seetharaman <prem@u.northwestern.edu>
2018-07-31 03:33:29 -0500

    added musdb

--- pipelines/prepare-data/prepare-musdb.sh
+++ pipelines/prepare-data/prepare-musdb.sh
@@ -2,4 +2,8 @@
 # [wf] execute setup stage
 
 mkdir -p raw
+mkdir -p raw/musdb
+#unzip raw/musdb18.zip
+tar xvf raw/musdb18.zip -C raw/musdb
 docker pull faroit/sigsep-mus-io
+docker run --rm -v `pwd`/raw/musdb:/data faroit/sigsep-mus-io /scripts/decode.sh
