9a5f748955837802655548196660398af153209e
Prem Seetharaman <prem@u.northwestern.edu>
2018-10-17 12:02:14 -0400

    more updates

---
+++ pipelines/create-anaconda-environment/buid-conda-environment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source activate prem
+if [ $? -eq 1 ]; then
+    conda create -y --name prem
+    source activate prem
+fi
+
+conda install -y ffmpeg
+conda install -y -c conda-forge sox
+
+# Install required packages.
+pip install -r requirements.txt
+
+git clone https://github.com/pseeth/scaper
+cd scaper
+git checkout source_separation
+pip install -e .
+
+pip install -U git+https://github.com/pseeth/audio_embed.git
+conda install -y pytorch torchvision -c pytorch
+
+echo "0" > ../DOCKER
