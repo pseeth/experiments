53b4bdd48fbcf39fdb50788acb25471ffcecd15d
Prem Seetharaman <prem@u.northwestern.edu>
2018-10-21 13:09:14 -0500

    added gmm-based spatial clustering

--- code/gmm_spatial_clustering.py
+++ code/gmm_spatial_clustering.py
@@ -1,6 +1,6 @@
+import numpy as np
 from sklearn.mixture import GaussianMixture
 import librosa
-import numpy as np
 
 def transform(data, n_fft, hop_length):
     n = len(data)
@@ -9,13 +9,15 @@ def transform(data, n_fft, hop_length):
     log_spec = librosa.amplitude_to_db(np.abs(stft), ref=np.max)
     return log_spec, stft
 
-def mask_mixture(source_mask, mix):
+
+def mask_mixture(source_mask, mix, n_fft, hop_length):
     n = len(mix)
     mix = librosa.util.fix_length(mix, n + n_fft // 2)
     mix_stft = librosa.stft(mix, n_fft=n_fft, hop_length=hop_length)
     masked_mix = mix_stft * source_mask
     source = librosa.istft(masked_mix, hop_length=hop_length, length=n)
     return source
+
 
 def multichannel_stft(mix, n_fft, hop_length):
     mix_stft = []
@@ -24,7 +26,8 @@ def multichannel_stft(mix, n_fft, hop_length):
         mix_stft.append(_mix_stft)
     return np.stack(mix_stft, axis=-1)
 
-def extract_spatial_features(mix_stft):
+
+def extract_spatial_features(mix_stft, n_fft, sr):
     interlevel_difference = np.abs((mix_stft[:, :, 0] + 1e-8) ** 2 / (mix_stft[:, :, 1] + 1e-8)) ** 2
     interlevel_difference = 10 * np.log10(interlevel_difference + 1e-8)
 
@@ -32,29 +35,28 @@ def extract_spatial_features(mix_stft):
     interphase_difference = np.angle(mix_stft[:, :, 0] * np.conj(mix_stft[:, :, 1])) / (frequencies + 1.0)
     return interlevel_difference, interphase_difference
 
-def gmm_spatial_clustering(mix, num_sources, n_fft, hop_length, verbose=True):
+
+def gmm_spatial_clustering(mix, sr, num_sources, n_fft, hop_length, verbose=True):
     mix_stft = multichannel_stft(mix, n_fft, hop_length)
 
     weights = np.mean(20 * np.log10(np.abs(mix_stft) + 1e-8), axis=-1).flatten()
     weights -= weights.min()
     weights /= weights.max()
-    weights = weights > .5
 
-    interlevel_difference, interphase_difference = extract_spatial_features(mix_stft)
+    interlevel_difference, interphase_difference = extract_spatial_features(mix_stft, n_fft, sr)
     features = np.vstack([np.sin(interphase_difference).flatten(),
                           np.cos(interphase_difference).flatten()]).T
-    features = (features.T * weights).T
+    features_fit = features[weights > .5]
 
-    data = features
     clusterer = GaussianMixture(n_components=2, covariance_type='full', init_params='kmeans')
-    clusterer.fit(data)
-    assignments = clusterer.predict_proba(data)
+    clusterer.fit(features_fit)
+    assignments = clusterer.predict_proba(features)
     assignments = assignments.reshape(mix_stft.shape[:-1] + (-1,))
-    scores = np.exp(clusterer.score_samples(data).reshape(mix_stft.shape[:-1]))
+    scores = np.exp(clusterer.score_samples(features).reshape(mix_stft.shape[:-1]))
     sources = []
     for i in range(assignments.shape[-1]):
         mask = assignments[:, :, i]
-        source = np.vstack([mask_mixture(mask.T, mix[i]) for i in range(mix.shape[0])])
+        source = np.vstack([mask_mixture(mask.T, mix[i], n_fft, hop_length) for i in range(mix.shape[0])])
         sources.append(source)
 
-    return sources, clusterer.bic(data), scores
+    return sources, clusterer.bic(features), scores
