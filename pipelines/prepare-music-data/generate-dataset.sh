1aaefd262c47527b2a763c91a61ae4f564b64007
pseeth <prem@stark.local>
2018-12-13 01:17:38 -0600

    getting rid of venv

--- pipelines/prepare-music-data/generate-dataset.sh
+++ pipelines/prepare-music-data/generate-dataset.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
-USE_DOCKER=`cat ../DOCKER`
 rm -rf data/generated
 
-if [ $USE_DOCKER -eq 1 ]; then
-    docker stop -t 0 data_generation || true
-    docker run --rm --workdir=/pipeline -v `pwd`:/pipeline \
-      -v `pwd`/../../data:/pipeline/data \
-      --entrypoint python \
-      --name data_generation \
-      --user $UID \
-      thesis scripts/create_incoherent_music.py
-fi
-
-if [ $USE_DOCKER -eq 0 ]; then
-    source activate prem
-    python scripts/create_incoherent_music.py
-fi
+docker stop -t 0 data_generation || true
+docker run --rm --workdir=/pipeline -v `pwd`:/pipeline \
+    -v `pwd`/../../data:/pipeline/data \
+    --entrypoint python \
+    --name data_generation \
+    --user $UID \
+    thesis scripts/create_incoherent_music.py
